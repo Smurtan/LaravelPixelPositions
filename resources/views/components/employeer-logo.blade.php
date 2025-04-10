972c55edd7190cba252614941b0dd09dce8bae7c
Vycheslav <smirnovvycheslavfr@gmail.com>
2025-04-10 16:39:31 +0300

    Made the layout of the main page

---
+++ resources/views/components/employeer-logo.blade.php
@@ -0,0 +1,3 @@
+@props(['width' => 90])
+
+<img src="https://loremflickr.com/{{ $width }}/{{ $width }}?random={{ random_int(1, 999999) }}" alt="" class="rounded-xl">
---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<x-panel class="flex gap-x-6">
+    <div class="">
+        <x-employeer-logo/>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasta</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div class="">
+        <x-tag size="small">Tab</x-tag>
+        <x-tag size="small">Tab</x-tag>
+        <x-tag size="small">Tab</x-tag>
+    </div>
+</x-panel>
